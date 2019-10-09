8eebc0441cf8cd99ee1b36e35268067cbdb5b89b
robinno <robin.nollet@student.kuleuven.be>
2019-10-09 16:53:40 +0200

    voor pull

--- top/toplevel.c
+++ top/toplevel.c
@@ -1,6 +1,26 @@
 #include "./headerfiles/toplevel.h"
 #include "fsl_debug_console.h"
 
-void toplevel(){
-	PRINTF("in toplevel");
+#include "MK64F12.h"
+
+void delay() {
+	for (long i = 0; i < 1000000; i++) {
+	}
+}
+
+void toplevel() {
+	printf("in toplevel \n\r");
+
+	SIM->SCGC5 |= 0x400; //enable Port B Clock Gate Control
+	PORTB->PCR[21] = 0x100; //blue led, configured as Alternative 1 (GPIO)
+	GPIOB->PDDR |= (1 << 21); //setting the bit 21 of the port B as output
+	GPIOB->PDOR = (0 << 21); //turn off blue led
+	printf("print some debugging info, can be seen in console window.\n\r");
+	while (1) {
+		GPIOB->PDOR = (0 << 21); //turn off blue led
+		delay();
+		GPIOB->PDOR = (1 << 21); //turn on blue led
+		delay();
+	}
+
 }
